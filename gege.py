f1810088fcf8cdca59a6f4c02c18df7d268d1eb6
Melmariemejos11 <melmariemejos25@gmail.com>
2024-09-28 11:01:47 +0800

    mytask

---
+++ gege.py
@@ -0,0 +1,17 @@
+age = [-12,13,-14,15,-16]
+
+username = ["juan","pedro","james","sagaad","fuego"]
+
+
+for dataage in age:
+    if dataage > 0:
+        print (str(dataage) + "positive")
+    elif dataage < 0:
+        print(str(dataage) + "negative")
+
+for datauser in username:
+    if datauser == "james":
+        print("james gwapo") 
+    else:
+        print("james none")
+
